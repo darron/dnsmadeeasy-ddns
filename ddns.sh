e612dccc598b7f6d4bc5504472f43f5e77b974b8
Darron Froese <darron@froese.org>
2014-06-03 13:27:31 -0600

    Show URL for debug.

--- ddns.sh
+++ ddns.sh
@@ -30,6 +30,7 @@ do
 
   if [ "$CURRENT_DNS_IP" != "$IP_ADDRESS" ]; then
     URL="https://www.dnsmadeeasy.com/servlet/updateip?username=$DME_USER&password=$DME_PASS&id=$DME_ID&ip=$IP_ADDRESS"
+    echo "URL: $URL"
     if wget -q -O /proc/self/fd/1 $URL | grep success > /dev/null; then
     	echo "DNS Record Updated Successfully" > /proc/self/fd/1
     else
