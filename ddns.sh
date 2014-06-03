48d36b9c16fe437bc11886648078816faac03b09
Darron Froese <darron@froese.org>
2014-06-03 13:24:09 -0600

    Fix URL and remove escaping.

--- ddns.sh
+++ ddns.sh
@@ -29,8 +29,7 @@ do
   CURRENT_DNS_IP=$($DIG_COMMAND)
 
   if [ "$CURRENT_DNS_IP" != "$IP_ADDRESS" ]; then
-    URL="https://www.dnsmadeeasy.com/servlet/updateip?username=$DME_USER\&password=$DME_PASS\&id=$DME_ID\&ip=$IP_ADDRESS"
-    echo "URL: $URL"
+    URL="https://www.dnsmadeeasy.com/servlet/updateip?username=$DME_USER&password=$DME_PASS&id=$DME_ID&ip=$IP_ADDRESS"
     if wget -q -O /proc/self/fd/1 $URL | grep success > /dev/null; then
     	echo "DNS Record Updated Successfully" > /proc/self/fd/1
     else
